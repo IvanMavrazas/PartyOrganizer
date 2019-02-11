a30c535c657018d3007c692b40e8f50bbfcb56bc
Ivan Mavrazas <mavrazas@gmail.com>
2019-02-11 23:39:05 +0100

    Add delegate and datasource methods to MemberVC Designed MemberTableViewCell

--- Party Organizer/Controllers/MembersViewController.swift
+++ Party Organizer/Controllers/MembersViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 
-class MembersViewController: UIViewController {
+class MembersViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var membersArray = ["John","Sarah"]
 
+    @IBOutlet weak var membersTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        membersTableView.delegate = self
+        membersTableView.dataSource = self
+        
+        membersTableView.register(UINib(nibName: "MemberTableViewCell", bundle: nil), forCellReuseIdentifier: "MemberTableViewCell")
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.topItem?.title = "Members"
     }
 
+    
+    //MARK - TableView DataSource Methods
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemberTableViewCell", for: indexPath) as! MemberTableViewCell
+        cell.textLabel?.text = membersArray[indexPath.row]
+        print(membersArray[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return membersArray.count
+    }
 }
---
+++ Party Organizer/View/MemberCell/MemberTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MemberTableViewCell.swift
+//  Party Organizer
+//
+//  Created by Ivan Mavrazas on 2/11/19.
+//  Copyright © 2019 Ivan Mavrazas. All rights reserved.
+//
+
+import UIKit
+
+class MemberTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var memberImage: UIImageView!
+    @IBOutlet weak var memberName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+    
+}
