d0ac67de732bf827d4d074b7dac8114bb09778d8
Ivan Mavrazas <mavrazas@gmail.com>
2019-02-11 21:22:30 +0100

    Create all ViewControllers in StoryBoard

---
+++ Party Organizer/PartiesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Party Organizer
+//
+//  Created by Ivan Mavrazas on 2/11/19.
+//  Copyright © 2019 Ivan Mavrazas. All rights reserved.
+//
+
+import UIKit
+
+class PartiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Party Organizer/PartyMemberPreviewScreen.swift
@@ -0,0 +1,30 @@
+//
+//  PartyMemberPreviewScreen.swift
+//  Party Organizer
+//
+//  Created by Ivan Mavrazas on 2/11/19.
+//  Copyright © 2019 Ivan Mavrazas. All rights reserved.
+//
+
+import UIKit
+
+class PartyMemberPreviewScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
