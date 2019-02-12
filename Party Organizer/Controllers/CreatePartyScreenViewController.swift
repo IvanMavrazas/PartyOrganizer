75163eb16162a26debb850f484ba9af6472509fa
Ivan Mavrazas <mavrazas@gmail.com>
2019-02-12 15:18:41 +0100

    remove startDateAndTimeTF

--- Party Organizer/Controllers/CreatePartyScreenViewController.swift
+++ Party Organizer/Controllers/CreatePartyScreenViewController.swift
@@ -8,30 +8,65 @@
 
 import UIKit
 
-class CreatePartyScreenViewController: UIViewController {
+class CreatePartyScreenViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var membersArray = ["Mary","Bill"]
 
     @IBOutlet weak var partyNameLabel: UILabel!
     @IBOutlet weak var partyNameTextField: UITextField!
     @IBOutlet weak var startDateLabel: UILabel!
     @IBOutlet weak var startTimeLabel: UILabel!
-    @IBOutlet weak var startDateAndTimeTextField: UITextField!
+    
     @IBOutlet weak var membersLabel: UILabel!
     @IBOutlet weak var numberOfMembersLabel: UILabel!
     
     @IBOutlet weak var membersTableView: UITableView!
+    @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        membersTableView.delegate = self
+        membersTableView.dataSource = self
+        
+        datePicker.isHidden = true
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "memberCell", for: indexPath)
+        cell.textLabel?.text = membersArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      
+        return membersArray.count
+    }
 
     @IBAction func saveNewPartyPressed(_ sender: UIBarButtonItem) {
         
     }
     
     @IBAction func membersButtonPressed(_ sender: UIButton) {
+        guard let partyMemberPreviewScreen = storyboard?.instantiateViewController(withIdentifier: "PartyMemberPreviewScreen") as? PartyMemberPreviewScreen else {
+            fatalError("Couldn't load PartyMemberPreviewScreen")
+        }
+        show(partyMemberPreviewScreen, sender: self)
     }
     
+    @IBAction func startDateAndTimePressed(_ sender: UIButton) {
+        datePicker.isHidden = false
+        
+        
+    }
+    func setPartyDateAndTime() {
+        
+    }
     
+    func showDatePicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy"
+        startDateLabel.text = formatter.string(from: datePicker.date)
+    }
 }
+
