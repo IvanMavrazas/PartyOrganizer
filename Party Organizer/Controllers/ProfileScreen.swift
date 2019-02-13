c9cc9d61df58b7c58a913284e56f46baeea402a9
Ivan Mavrazas <mavrazas@gmail.com>
2019-02-13 15:43:09 +0100

    Create UI in ProfileScreen VC Connect IBOutlets and addToPartyPressed

--- Party Organizer/Controllers/ProfileScreen.swift
+++ Party Organizer/Controllers/ProfileScreen.swift
@@ -12,6 +12,12 @@ class ProfileScreen: UIViewController {
 
     var profiles: Profiles?
     
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var gender: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var about: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,5 +25,8 @@ class ProfileScreen: UIViewController {
     }
     
 
-
+    @IBAction func addToPartyPressed(_ sender: Any) {
+        
+    }
+    
 }
