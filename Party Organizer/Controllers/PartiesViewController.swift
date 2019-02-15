7654a5519092085865ef4cb43bdc32511a025881
Ivan Mavrazas <mavrazas@gmail.com>
2019-02-15 22:14:09 +0100

    Successfully implemented showEmptyScreen function when there is no data in Parties tableview

--- Party Organizer/Controllers/PartiesViewController.swift
+++ Party Organizer/Controllers/PartiesViewController.swift
@@ -2,51 +2,68 @@
 //  Parties2ViewController.swift
 //  Party Organizer
 //
-//  Created by Ivan Mavrazas on 2/12/19.
+//  Created by Ivan Mavrazas on 2/15/19.
 //  Copyright © 2019 Ivan Mavrazas. All rights reserved.
 //
 
 import UIKit
-import Kingfisher
 
-class PartiesViewController: UITableViewController,PartySavedDelegate {
-    
+class PartiesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
     // CreatePartyVC
     
     var parties = [Party]()
+    @IBOutlet weak var emptyScreenView: UIView!
     
-    @IBOutlet var partiesTableView: UITableView!
-
+    
+    @IBOutlet weak var partiesTableView: UITableView!
+    
     // Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        partiesTableView.delegate = self
+        partiesTableView.dataSource = self
+        
         partiesTableView.register(UINib(nibName: "PartyTableViewCell", bundle: nil), forCellReuseIdentifier: "PartyTableViewCell")
+        showEmptyScreen()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        partiesTableView.delegate = self
         navigationController?.navigationBar.topItem?.title = "Parties"
         partiesTableView.reloadData()
     }
     
     //MARK:  TableView DataSource Methods
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PartyTableViewCell", for: indexPath) as! PartyTableViewCell
         cell.party = parties[indexPath.row]
         
         return cell
     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return parties.count
-    }    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+            return parties.count
+
+        
+    }
     
-    //MARK:  Buttons and functions
-    
+
+    //MARK: Buttons
+   
     @IBAction func addPartyPressed(_ sender: UIBarButtonItem) {
         addParty()
     }
+    
+    @IBAction func createButonPressed(_ sender: UIButton) {
+        addParty()
+    }
+    
+    //Functions
     
     func addParty() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -57,10 +74,19 @@ class PartiesViewController: UITableViewController,PartySavedDelegate {
         createPartyVC.hidesBottomBarWhenPushed = true
         show(createPartyVC, sender: self)
     }
-    
+
+    func showEmptyScreen() {
+        if partiesTableView.numberOfRows(inSection: parties.count) == 0 {
+            emptyScreenView.isHidden = false
+        }
+    }
+}
+
+extension PartiesViewController: PartySavedDelegate {
     func userSavedParty(partyName: String, partyDateAndTime: String, partyDescription: String) {
         let oneParty = Party(partyName: partyName, partyDate: partyDateAndTime, partyDescription: partyDescription)
         parties.append(oneParty)
-        print(partyName)
+        print(oneParty)
     }
+
 }
